aa2c5a788754c4a22f27459491c6660c70c32286
Dante Spencer <spencerdantel@gmail.com>
2019-10-06 14:32:08 +0000

    Done.

---
+++ app/models/dog..rb
@@ -0,0 +1,16 @@
+class Dog
+  attr_accessor :name, :breed, :age
+  @@all = []
+  
+  def initialize
+    @name = name
+    @breed = breed
+    @age = age
+  end
+  
+  def self.all
+    @@ll << self
+  end
+  
+
+
